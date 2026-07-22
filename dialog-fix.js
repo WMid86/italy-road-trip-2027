4431ec15f1b62d498e2af078fec17a6cd2a894e8
WMid86 <160694102+WMid86@users.noreply.github.com>
2026-07-23 00:20:20 +0800

    Load destination photos and planning links

--- dialog-fix.js
+++ dialog-fix.js
@@ -1,7 +1,18 @@
 (() => {
-  if (typeof HTMLDialogElement === 'undefined') return;
-  const nativeShowModal = HTMLDialogElement.prototype.showModal;
-  HTMLDialogElement.prototype.showModal = function safeShowModal() {
-    if (!this.open) nativeShowModal.call(this);
-  };
+  if (typeof HTMLDialogElement !== 'undefined') {
+    const nativeShowModal = HTMLDialogElement.prototype.showModal;
+    HTMLDialogElement.prototype.showModal = function safeShowModal() {
+      if (!this.open) nativeShowModal.call(this);
+    };
+  }
+
+  const stylesheet = document.createElement('link');
+  stylesheet.rel = 'stylesheet';
+  stylesheet.href = 'media.css';
+  document.head.appendChild(stylesheet);
+
+  const contentScript = document.createElement('script');
+  contentScript.src = 'content-links.js';
+  contentScript.async = false;
+  document.body.appendChild(contentScript);
 })();
